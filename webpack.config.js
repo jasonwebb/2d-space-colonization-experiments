7dd923938bbc5aa855871c02fe3d2de8e70c804b
Jason Webb <zen.webb@gmail.com>
2019-06-19 18:24:57 -0500

    Adding initial Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    basic: path.resolve('basic/js/entry.js')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    host: '127.0.0.1',
+    port: 9001,
+    publicPath: '/dist/',
+    contentBase: path.resolve('./'),
+    compress: true,
+    open: true,
+    watchContentBase: true
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve('dist')
+  }
+}
