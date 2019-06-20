229fe84fff6eece77e971f3411bf60257f2acdef
Jason Webb <zen.webb@gmail.com>
2019-06-20 17:39:47 -0500

    Implementing spatial index for segments, prepping for open/closed types, removing initializeTrunks

--- core/Network.js
+++ core/Network.js
@@ -1,4 +1,5 @@
 import Defaults from './Defaults';
+import KDBush from 'kdbush';
 
 export default class Network {
   constructor(ctx, settings) {
@@ -7,55 +8,88 @@ export default class Network {
 
     this.sources = [];   // sources are locations of auxin, the growth hormone
     this.segments = [];  // segments are discrete line segments that form veins
-    this.roots = [];     // roots are locations where growth begins
+
+    this.buildSpatialIndices();
   }
 
   update() {
-    if(!this.settings.IsPaused) {
-      // For each auxin source ...
-      for(let source of this.sources) {
-        let closestSegment = null,
-          record = this.settings.AttractionDistance;
+    // Skip iteration if paused
+    if(this.settings.IsPaused) {
+      return;
+    }
 
-        // Check if any vein segment is near enough to trigger removal of auxin source
-        // TODO: implement knn searching by putting segments into spatial index
-        for(let segment of this.segments) {
-          let distance = source.position.distance(segment.position);
+    // For each auxin source ...
+    for(let [sourceID, source] of this.sources.entries()) {
+      let closestSegment = null,
+        record = this.settings.AttractionDistance;
 
-          if(distance < this.settings.KillDistance) {
-            source.reached = true;
-            closestSegment = null;
-            break;
-          } else if(distance < record) {
-            closestSegment = segment;
-            record = distance;
+      // Do knn search to find all segments within AttractionDistance of this auxin source
+      let closestSegments = this.segmentsIndex.within(source.position.x, source.position.y, this.settings.AttractionDistance).map(id => this.segments[id]);
+
+      // Use slightly different algorithms depending on whether we want to make open or closed venation networks
+      switch(this.settings.VenationPattern) {
+        case "Open":
+          // Find the closest vein segment, and also check if any segment has reached its auxin source
+          for(let segment of closestSegments) {
+            let distance = source.position.distance(segment.position);
+
+            if(distance < this.settings.KillDistance) {
+              source.reached = true;
+              closestSegment = null;
+              break;
+            } else if(distance < record) {
+              closestSegment = segment;
+              record = distance;
+            }
           }
-        }
 
-        // "Bend" the vein growth towards the nearest branch
-        if(closestSegment != null) {
-          let nextDirection = source.position.subtract(closestSegment.position, true);
-          nextDirection.normalize();
-          closestSegment.direction.add(nextDirection);
-          closestSegment.count++;
-        }
-      }
+          // "Bend" the nearest vein segment towards this auxin source
+          if(closestSegment != null) {
+            let nextDirection = source.position.subtract(closestSegment.position, true);
+            nextDirection.normalize();
+            closestSegment.direction.add(nextDirection);
+            closestSegment.count++;
+          }
 
-      // Remove auxin sources as soon as a vein reaches them
-      for(let [index, source] of this.sources.entries()) {
-        if(source.reached) {
-          this.sources.splice(index, 1);
-        }
-      }
+          break;
 
-      for(let segment of this.segments) {
-        if(segment.count > 0) {
-          segment.direction.divide(segment.count + 1);
-          this.segments.push(segment.nextSegment());
-          segment.reset();
-        }
+        case "Closed": 
+          // TODO: extend for "closed" venation by allowing this source to influence direction of ALL nearby segments, not just the closest one
+
+          for(let segment of closestSegments) {
+            let distance = source.position.distance(segment.position);
+
+            if(distance < this.settings.KillDistance) {
+              // TODO: associate this segment (and all its descendent segments) with this auxin source for tracking later
+            }
+
+            // TODO: find the "tips" of each vein segment moving towards this auxin source and "bend" them each towards it
+          }
+
+          break;
       }
     }
+
+    // Remove auxin sources as soon as a vein reaches them
+    // TODO: extend this logic for "closed" venation type by waiting until all veins influenced by this source reach the source
+    for(let [index, source] of this.sources.entries()) {
+      if(source.reached) {
+        this.sources.splice(index, 1);
+      }
+    }
+
+    // Add new vein segments to any segments that were influenced by an auxin source this 
+    // iteration, pointed towards the average location of all the auxin sources influencing it
+    for(let segment of this.segments) {
+      if(segment.count > 0) {
+        segment.direction.divide(segment.count + 1);
+        this.segments.push(segment.nextSegment());
+        segment.reset();
+      }
+    }
+
+    // Rebuild spatial indices
+    this.buildSpatialIndices();
   }
 
   draw() {
@@ -74,37 +108,9 @@ export default class Network {
     }
   }
 
-  initializeTrunks() {
-    // For each root vein segment ...
-    for(let root of this.roots) {
-      let currentSegment = root;
-      let found = false;
-
-      // Start searching for auxin sources ...
-      while(!found) {
-        // Check if root segment is within kill distance of any auxin source
-        for(let source of this.sources) {
-          const distance = currentSegment.position.distance(source.position);
-
-          if(distance < this.settings.KillDistance) {
-            found = true;
-          }
-        }
-
-        // If not in kill range of a source, grow the vein
-        if(!found) {
-          let nextSegment = currentSegment.nextSegment();
-          currentSegment = nextSegment;
-          this.segments.push(currentSegment);
-        }
-
-        // Prevent infinite loops caused by too few auxin sources
-        if(currentSegment.position.x < 0 || currentSegment.position.x > window.innerWidth || currentSegment.position.y < 0 || currentSegment.position.y > window.innerHeight) {
-          console.log("Missed all sources");
-          break;
-        }
-      }
-    }
+  buildSpatialIndices() {
+    this.sourcesIndex = new KDBush(this.sources, p => p.position.x, p => p.position.y);
+    this.segmentsIndex = new KDBush(this.segments, p => p.position.x, p => p.position.y);
   }
 
   toggleVeins() {
