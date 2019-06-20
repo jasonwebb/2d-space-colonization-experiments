ebe7e1847c349a9e764be70634e44a230541bef9
Jason Webb <zen.webb@gmail.com>
2019-06-19 21:49:13 -0500

    Flipping logic of open/closed venation flag, adding IsPaused

--- core/Defaults.js
+++ core/Defaults.js
@@ -3,10 +3,11 @@ export default {
    * Simulation configurations
    */
 
-  ClosedCells: true,        // venation can be "open" (false) or "closed" (true)
-  SegmentLength: 10,        // length of each vein segment. Higher numbers mean smoother lines, but more computation cost
+  OpenCells: true,          // venation can be "open" (true) or "closed" (false)
+  SegmentLength: 10,        // length of each vein segment. Smaller numbers mean smoother lines, but more computation cost
   AttractionDistance: 100,  // radius of influence (d_i) around each auxin source that attracts vein segments
   KillDistance: 10,         // distance (d_k) between auxin sources and segments when segments are ended
+  IsPaused: false,
 
 
   /**
@@ -21,7 +22,7 @@ export default {
   VeinRenderMode: 'Lines',  // draw vein segments as "Lines" or "Dots"
 
   // Veins
-  VeinThickness: 2,
+  MinimumVeinThickness: 1,
 
   // Auxin sources
   AuxinRadius: 2
