0c75169848cd9020579d92d84689a3e891e90ab8
Jason Webb <zen.webb@gmail.com>
2019-11-17 22:29:09 -0600

    Simpliying redundant code for network setup/resets

--- bounds/js/entry.js
+++ bounds/js/entry.js
@@ -29,14 +29,8 @@ let setup = () => {
   // Initialize simulation object
   network = new Network(ctx);
 
-  // Add bounding shape
-  addBounds();
-
-  // Add auxin sources for veins to move towards
-  addSources();
-
-  // Add root veins
-  addRootVeins();
+  // Add the bounds, sources, and root nodes
+  resetNetwork();
 
   // Set up common keyboard interaction listeners
   setupKeyListeners(network);
@@ -45,121 +39,128 @@ let setup = () => {
   requestAnimationFrame(update);
 }
 
-let addBounds = () => {
-  switch(currentBoundsShape) {
-    case SQUARE:
-      network.bounds = getSquareBounds();
-      break;
-
-    case CIRCLE:
-      network.bounds = getCircleBounds();
-      break;
-
-    case LEAF:
-      network.bounds = getLeafBounds();
-      break;
-  }
+let resetNetwork = () => {
+  network.reset();
+  addBounds();
+  addSources();
+  addRootVeins();
 }
 
-  let getSquareBounds = () => {
-    const cx = window.innerWidth / 2;
-    const cy = window.innerHeight / 2;
-    const sideLength = 800;
+  let addBounds = () => {
+    switch(currentBoundsShape) {
+      case SQUARE:
+        network.bounds = getSquareBounds();
+        break;
 
-    return new Path(
-      [
-        [cx - sideLength/2, cy - sideLength/2],  // top left corner
-        [cx + sideLength/2, cy - sideLength/2],  // top right corner
-        [cx + sideLength/2, cy + sideLength/2],  // bottom right corner
-        [cx - sideLength/2, cy + sideLength/2]   // bottom left corner
-      ],
-      'Bounds',
-      ctx
-    );
+      case CIRCLE:
+        network.bounds = getCircleBounds();
+        break;
+
+      case LEAF:
+        network.bounds = getLeafBounds();
+        break;
+    }
   }
 
-  let getCircleBounds = () => {
-    const cx = window.innerWidth / 2;
-    const cy = window.innerHeight / 2;
-    const radius = 350;
-    const resolution = 100;
-    let points = [];
+    let getSquareBounds = () => {
+      const cx = window.innerWidth / 2;
+      const cy = window.innerHeight / 2;
+      const sideLength = 800;
 
-    for(let i = 0; i < resolution; i++) {
-      let angle = 2 * Math.PI * i / resolution;
-      let x = cx + Math.floor(radius * Math.cos(angle));
-      let y = cy + Math.floor(radius * Math.sin(angle));
-
-      points.push([x, y]);
+      return new Path(
+        [
+          [cx - sideLength/2, cy - sideLength/2],  // top left corner
+          [cx + sideLength/2, cy - sideLength/2],  // top right corner
+          [cx + sideLength/2, cy + sideLength/2],  // bottom right corner
+          [cx - sideLength/2, cy + sideLength/2]   // bottom left corner
+        ],
+        'Bounds',
+        ctx
+      );
     }
 
-    return new Path(points, 'Bounds', ctx);
-  }
+    let getCircleBounds = () => {
+      const cx = window.innerWidth / 2;
+      const cy = window.innerHeight / 2;
+      const radius = 350;
+      const resolution = 100;
+      let points = [];
 
-  let getLeafBounds = () => {
-    const cx = window.innerWidth / 2;
-    const cy = window.innerHeight / 2;
-    const shapeWidth = 900;
-    const shapeHeight = 900;
+      for(let i = 0; i < resolution; i++) {
+        let angle = 2 * Math.PI * i / resolution;
+        let x = cx + Math.floor(radius * Math.cos(angle));
+        let y = cy + Math.floor(radius * Math.sin(angle));
 
-    let polygon = SVGLoader.load(leaf)[0];
+        points.push([x, y]);
+      }
 
-    // Translate the design to the screen center
-    for(let point of polygon) {
-      point[0] = cx - shapeWidth/2 + point[0];
-      point[1] = cy - shapeHeight/2 + point[1];
+      return new Path(points, 'Bounds', ctx);
     }
 
-    return new Path(polygon, 'Bounds', ctx);
+    let getLeafBounds = () => {
+      const cx = window.innerWidth / 2;
+      const cy = window.innerHeight / 2;
+      const shapeWidth = 900;
+      const shapeHeight = 900;
+
+      let polygon = SVGLoader.load(leaf)[0];
+
+      // Translate the design to the screen center
+      for(let point of polygon) {
+        point[0] = cx - shapeWidth/2 + point[0];
+        point[1] = cy - shapeHeight/2 + point[1];
+      }
+
+      return new Path(polygon, 'Bounds', ctx);
+    }
+
+  let addSources = () => {
+    // Set up the auxin sources using pre-made patterns
+    let randomSources = SourcePatterns.getRandomSources(500, ctx, network.bounds);
+    let gridSources = SourcePatterns.getGridOfSources(80, 80, ctx, network.bounds);
+
+    network.sources = gridSources;
   }
 
-let addSources = () => {
-  // Set up the auxin sources using pre-made patterns
-  let randomSources = SourcePatterns.getRandomSources(500, ctx, network.bounds);
-  let gridSources = SourcePatterns.getGridOfSources(80, 80, ctx, network.bounds);
+  // Create the network with initial conditions
+  let addRootVeins = () => {
+    switch(currentBoundsShape) {
+      case SQUARE:
+      case CIRCLE:
+        // Add a set of random root veins throughout scene
+        for(let i=0; i<10; i++) {
+          network.addVeinNode(
+            new VeinNode(
+              null,
+              new Vec2(
+                random(window.innerWidth),
+                random(window.innerHeight)
+              ),
+              true,
+              ctx
+            )
+          );
+        }
 
-  network.sources = gridSources;
-}
+        break;
 
-// Create the network with initial conditions
-let addRootVeins = () => {
-  switch(currentBoundsShape) {
-    case SQUARE:
-    case CIRCLE:
-      // Add a set of random root veins throughout scene
-      for(let i=0; i<10; i++) {
+      case LEAF:
+        // Put a single root note at the base of the leaf
         network.addVeinNode(
           new VeinNode(
             null,
             new Vec2(
-              random(window.innerWidth),
-              random(window.innerHeight)
+              window.innerWidth / 2 - 5,
+              window.innerHeight / 2 + 220
             ),
             true,
             ctx
           )
         );
-      }
 
-      break;
-
-    case LEAF:
-      // Put a single root note at the base of the leaf
-      network.addVeinNode(
-        new VeinNode(
-          null,
-          new Vec2(
-            window.innerWidth / 2 - 5,
-            window.innerHeight / 2 + 220
-          ),
-          true,
-          ctx
-        )
-      );
-
-      break;
+        break;
+    }
   }
-}
 
 // Main program loop
 let update = (timestamp) => {
@@ -174,10 +175,7 @@ document.addEventListener('keypress', (e) => {
   switch(e.key) {
     // r = reset simulation by reinitializing the network with initial conditions
     case 'r':
-      network.reset();
-      addBounds();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
 
     // b = toggle visibility of bounds
@@ -187,26 +185,17 @@ document.addEventListener('keypress', (e) => {
 
     case '1':
       currentBoundsShape = SQUARE;
-      network.reset();
-      addBounds();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
 
     case '2':
       currentBoundsShape = CIRCLE;
-      network.reset();
-      addBounds();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
 
     case '3':
       currentBoundsShape = LEAF;
-      network.reset();
-      addBounds();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
   }
 });
--- obstacles/js/entry.js
+++ obstacles/js/entry.js
@@ -30,17 +30,8 @@ let setup = () => {
   // Initialize simulation object
   network = new Network(ctx);
 
-  // Add bounding shape
-  addBounds();
-
-  // Add obstacles to avoid
-  addObstacles();
-
-  // Add auxin sources for veins to move towards
-  addSources();
-
-  // Add root veins
-  addRootVeins();
+  // Add the bounds, sources, and root nodes
+  resetNetwork();
 
   // Set up common keyboard interaction listeners
   setupKeyListeners(network);
@@ -49,211 +40,219 @@ let setup = () => {
   requestAnimationFrame(update);
 }
 
-let addBounds = () => {
-  switch(currentBoundsShape) {
-    case TRIANGLE:
-      network.bounds = getTriangleBounds();
-      break;
-
-    case SQUARE:
-      network.bounds = getSquareBounds();
-      break;
-
-    case CIRCLE:
-      network.bounds = getCircleBounds();
-      break;
-
-    case LEAF:
-      network.bounds = getLeafBounds();
-      break;
-  }
+let resetNetwork = () => {
+  network.reset();
+  addBounds();
+  addObstacles();
+  addSources();
+  addRootVeins();
 }
 
-  let getTriangleBounds = () => {
-    const cx = window.innerWidth / 2;
-    const cy = window.innerHeight / 2;
+  let addBounds = () => {
+    switch(currentBoundsShape) {
+      case TRIANGLE:
+        network.bounds = getTriangleBounds();
+        break;
 
-    return new Path(
-      [
-        [cx - 400, cy + 300],
-        [cx, cy - 350],
-        [cx + 400, cy + 300]
-      ],
-      'Bounds',
-      ctx
-    )
+      case SQUARE:
+        network.bounds = getSquareBounds();
+        break;
+
+      case CIRCLE:
+        network.bounds = getCircleBounds();
+        break;
+
+      case LEAF:
+        network.bounds = getLeafBounds();
+        break;
+    }
   }
 
-  let getSquareBounds = () => {
-    const cx = window.innerWidth / 2;
-    const cy = window.innerHeight / 2;
-    const sideLength = 800;
+    let getTriangleBounds = () => {
+      const cx = window.innerWidth / 2;
+      const cy = window.innerHeight / 2;
 
-    return new Path(
-      [
-        [cx - sideLength/2, cy - sideLength/2],  // top left corner
-        [cx + sideLength/2, cy - sideLength/2],  // top right corner
-        [cx + sideLength/2, cy + sideLength/2],  // bottom right corner
-        [cx - sideLength/2, cy + sideLength/2]   // bottom left corner
-      ],
-      'Bounds',
-      ctx
-    );
-  }
-
-  let getCircleBounds = () => {
-    return new Path(
-      getCircleOfPoints(
-        window.innerWidth / 2,    // cx
-        window.innerHeight / 2,   // cy
-        350,                      // radius
-        100                       // resolution
-      ),
-      'Bounds',
-      ctx
-    );
-  }
-
-  let getLeafBounds = () => {
-    const cx = window.innerWidth / 2;
-    const cy = window.innerHeight / 2;
-    const shapeWidth = 900;
-    const shapeHeight = 900;
-
-    let polygon = SVGLoader.load(leaf)[0];
-
-    // Translate the design to the screen center
-    for(let point of polygon) {
-      point[0] = cx - shapeWidth/2 + point[0];
-      point[1] = cy - shapeHeight/2 + point[1];
+      return new Path(
+        [
+          [cx - 400, cy + 300],
+          [cx, cy - 350],
+          [cx + 400, cy + 300]
+        ],
+        'Bounds',
+        ctx
+      )
     }
 
-    return new Path(polygon, 'Bounds', ctx);
+    let getSquareBounds = () => {
+      const cx = window.innerWidth / 2;
+      const cy = window.innerHeight / 2;
+      const sideLength = 800;
+
+      return new Path(
+        [
+          [cx - sideLength/2, cy - sideLength/2],  // top left corner
+          [cx + sideLength/2, cy - sideLength/2],  // top right corner
+          [cx + sideLength/2, cy + sideLength/2],  // bottom right corner
+          [cx - sideLength/2, cy + sideLength/2]   // bottom left corner
+        ],
+        'Bounds',
+        ctx
+      );
+    }
+
+    let getCircleBounds = () => {
+      return new Path(
+        getCircleOfPoints(
+          window.innerWidth / 2,    // cx
+          window.innerHeight / 2,   // cy
+          350,                      // radius
+          100                       // resolution
+        ),
+        'Bounds',
+        ctx
+      );
+    }
+
+    let getLeafBounds = () => {
+      const cx = window.innerWidth / 2;
+      const cy = window.innerHeight / 2;
+      const shapeWidth = 900;
+      const shapeHeight = 900;
+
+      let polygon = SVGLoader.load(leaf)[0];
+
+      // Translate the design to the screen center
+      for(let point of polygon) {
+        point[0] = cx - shapeWidth/2 + point[0];
+        point[1] = cy - shapeHeight/2 + point[1];
+      }
+
+      return new Path(polygon, 'Bounds', ctx);
+    }
+
+  let addObstacles = () => {
+    // Ten random circles
+    // for(let i=0; i<10; i++) {
+    //   obstacles.push(
+    //     new Path(
+    //       getCircleOfPoints(
+    //         window.innerWidth / 2 + random(-300,300),
+    //         window.innerHeight / 2 + random(-300,300),
+    //         random(20,70),
+    //         100
+    //       ),
+    //       'Obstacle',
+    //       ctx
+    //     )
+    //   );
+    // }
+
+    // Single circle in center
+    network.obstacles.push(
+      new Path(
+        getCircleOfPoints(
+          window.innerWidth / 2,
+          window.innerHeight / 2 + 90,
+          200,
+          100
+        ),
+        'Obstacle',
+        ctx
+      )
+    );
   }
 
-let addObstacles = () => {
-  // Ten random circles
-  // for(let i=0; i<10; i++) {
-  //   obstacles.push(
-  //     new Path(
-  //       getCircleOfPoints(
-  //         window.innerWidth / 2 + random(-300,300),
-  //         window.innerHeight / 2 + random(-300,300),
-  //         random(20,70),
-  //         100
-  //       ),
-  //       'Obstacle',
-  //       ctx
-  //     )
-  //   );
-  // }
+  let addSources = () => {
+    // Set up the auxin sources using pre-made patterns
+    let randomSources = SourcePatterns.getRandomSources(500, ctx, network.bounds, network.obstacles);
+    let gridSources = SourcePatterns.getGridOfSources(200, 200, ctx, network.bounds, network.obstacles);
 
-  // Single circle in center
-  network.obstacles.push(
-    new Path(
-      getCircleOfPoints(
-        window.innerWidth / 2,
-        window.innerHeight / 2 + 90,
-        200,
-        100
-      ),
-      'Obstacle',
-      ctx
-    )
-  );
-}
+    network.sources = gridSources;
+  }
 
-let addSources = () => {
-  // Set up the auxin sources using pre-made patterns
-  let randomSources = SourcePatterns.getRandomSources(500, ctx, network.bounds, network.obstacles);
-  let gridSources = SourcePatterns.getGridOfSources(200, 200, ctx, network.bounds, network.obstacles);
+  // Create the network with initial conditions
+  let addRootVeins = () => {
+    const cx = window.innerWidth/2;
+    const cy = window.innerHeight/2;
 
-  network.sources = gridSources;
-}
+    switch(currentBoundsShape) {
+      case TRIANGLE:
+        network.addVeinNode(
+          new VeinNode(
+            null,
+            new Vec2(cx - 340, cy + 290),
+            true,
+            ctx
+          )
+        );
 
-// Create the network with initial conditions
-let addRootVeins = () => {
-  const cx = window.innerWidth/2;
-  const cy = window.innerHeight/2;
+        // network.addVeinNode(
+        //   new VeinNode(
+        //     null,
+        //     new Vec2(cx, cy - 300),
+        //     true,
+        //     ctx
+        //   )
+        // );
 
-  switch(currentBoundsShape) {
-    case TRIANGLE:
-      network.addVeinNode(
-        new VeinNode(
-          null,
-          new Vec2(cx - 340, cy + 290),
-          true,
-          ctx
-        )
-      );
+        // network.addVeinNode(
+        //   new VeinNode(
+        //     null,
+        //     new Vec2(cx + 340, cy + 290),
+        //     true,
+        //     ctx
+        //   )
+        // );
 
-      // network.addVeinNode(
-      //   new VeinNode(
-      //     null,
-      //     new Vec2(cx, cy - 300),
-      //     true,
-      //     ctx
-      //   )
-      // );
+        break;
 
-      // network.addVeinNode(
-      //   new VeinNode(
-      //     null,
-      //     new Vec2(cx + 340, cy + 290),
-      //     true,
-      //     ctx
-      //   )
-      // );
+      case CIRCLE:
+        network.addVeinNode(
+          new VeinNode(
+            null,
+            new Vec2(cx, cy + 300),
+            true,
+            ctx
+          )
+        );
 
-      break;
+        break;
 
-    case CIRCLE:
-      network.addVeinNode(
-        new VeinNode(
-          null,
-          new Vec2(cx, cy + 300),
-          true,
-          ctx
-        )
-      );
+      case SQUARE:
+        // Add a set of random root veins throughout scene
+        for(let i=0; i<10; i++) {
+          network.addVeinNode(
+            new VeinNode(
+              null,
+              new Vec2(
+                random(window.innerWidth),
+                random(window.innerHeight)
+              ),
+              true,
+              ctx
+            )
+          );
+        }
 
-      break;
+        break;
 
-    case SQUARE:
-      // Add a set of random root veins throughout scene
-      for(let i=0; i<10; i++) {
+      case LEAF:
+        // Put a single root note at the base of the leaf
         network.addVeinNode(
           new VeinNode(
             null,
             new Vec2(
-              random(window.innerWidth),
-              random(window.innerHeight)
+              window.innerWidth / 2 - 5,
+              window.innerHeight / 2 + 220
             ),
             true,
             ctx
           )
         );
-      }
 
-      break;
-
-    case LEAF:
-      // Put a single root note at the base of the leaf
-      network.addVeinNode(
-        new VeinNode(
-          null,
-          new Vec2(
-            window.innerWidth / 2 - 5,
-            window.innerHeight / 2 + 220
-          ),
-          true,
-          ctx
-        )
-      );
-
-      break;
+        break;
+    }
   }
-}
 
 // Main program loop
 let update = (timestamp) => {
@@ -268,11 +267,7 @@ document.addEventListener('keypress', (e) => {
   switch(e.key) {
     // r = reset simulation by reinitializing the network with initial conditions
     case 'r':
-      network.reset();
-      addBounds();
-      addObstacles();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
 
     // b = toggle visibility of bounds
@@ -287,38 +282,22 @@ document.addEventListener('keypress', (e) => {
 
     case '1':
       currentBoundsShape = TRIANGLE;
-      network.reset();
-      addBounds();
-      addObstacles();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
 
     case '2':
       currentBoundsShape = SQUARE;
-      network.reset();
-      addBounds();
-      addObstacles();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
 
     case '3':
       currentBoundsShape = CIRCLE;
-      network.reset();
-      addBounds();
-      addObstacles();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
 
     case '4':
       currentBoundsShape = LEAF;
-      network.reset();
-      addBounds();
-      addObstacles();
-      addSources();
-      addRootVeins();
+      resetNetwork();
       break;
   }
 });
