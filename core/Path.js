753b1761f8ee3ee02eaec81f8ed4a85894e3bac8
Jason Webb <zen.webb@gmail.com>
2019-11-18 21:51:51 -0600

    Removing unnecessary logic in Path

--- core/Path.js
+++ core/Path.js
@@ -16,36 +16,31 @@ export default class Path {
   }
 
   draw() {
-    if(
-      (this.type === 'Bounds' && this.settings.ShowBounds) ||
-      (this.type === 'Obstacle' && this.settings.ShowObstacles)
-    ) {
-      this.ctx.beginPath();
-      this.ctx.moveTo(this.polygon[0][0], this.polygon[0][1]);
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.polygon[0][0], this.polygon[0][1]);
 
-      for(let i = 0; i < this.polygon.length; i++) {
-        this.ctx.lineTo(this.polygon[i][0], this.polygon[i][1]);
-      }
-
-      this.ctx.lineTo(this.polygon[0][0], this.polygon[0][1]);
-
-      switch(this.type) {
-        case 'Bounds':
-          this.ctx.strokeStyle = this.settings.Colors.BoundsBorderColor;
-          this.ctx.lineWidth = this.settings.BoundsBorderThickness;
-          this.ctx.fillStyle = this.settings.Colors.BoundsFillColor;
-
-          this.ctx.stroke();
-          this.ctx.lineWidth = 1;
-
-          break;
-
-        case 'Obstacle':
-          this.ctx.fillStyle = this.settings.Colors.ObstacleFillColor;
-          break;
-      }
-
-      this.ctx.fill();
+    for(let i = 0; i < this.polygon.length; i++) {
+      this.ctx.lineTo(this.polygon[i][0], this.polygon[i][1]);
     }
+
+    this.ctx.lineTo(this.polygon[0][0], this.polygon[0][1]);
+
+    switch(this.type) {
+      case 'Bounds':
+        this.ctx.strokeStyle = this.settings.Colors.BoundsBorderColor;
+        this.ctx.lineWidth = this.settings.BoundsBorderThickness;
+        this.ctx.fillStyle = this.settings.Colors.BoundsFillColor;
+
+        this.ctx.stroke();
+        this.ctx.lineWidth = 1;
+
+        break;
+
+      case 'Obstacle':
+        this.ctx.fillStyle = this.settings.Colors.ObstacleFillColor;
+        break;
+    }
+
+    this.ctx.fill();
   }
 }
