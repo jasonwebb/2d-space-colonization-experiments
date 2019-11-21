f9a2d3b2e6c7f65972dc29aaeaba0cbae3609de2
Jason Webb <zen.webb@gmail.com>
2019-11-20 22:02:05 -0600

    Lowering AttractionDistance for better performance in all sketches

--- core/Defaults.js
+++ core/Defaults.js
@@ -7,7 +7,7 @@ export default {
 
   VenationType: 'Open',     // venation can be "Open" or "Closed"
   SegmentLength: 5,         // length of each vein segment. Smaller numbers mean smoother lines, but more computation cost
-  AttractionDistance: 200,  // radius of influence (d_i) around each auxin source that attracts vein segments
+  AttractionDistance: 50,  // radius of influence (d_i) around each auxin source that attracts vein segments
   KillDistance: 5,          // distance (d_k) between auxin sources and segments when segments are ended
   IsPaused: false,          // initial pause/unpause state
 
@@ -17,14 +17,14 @@ export default {
   */
 
   // Visibility toggles
-  ShowSources: false,           // toggled with 's'
-  ShowVeins: true,              // toggled with 'v'
-  ShowVeinTips: false,          // toggled with 't'
-  ShowAttractionZones: false,   // toggled with 'a'
-  ShowKillZones: false,         // toggled with 'k'
-  ShowInfluenceLines: false,    // toggled with 'i'
-  ShowBounds: true,             // toggled with 'b'
-  ShowObstacles: true,          // toggled with 'o'
+  ShowSources: false,          // toggled with 's'
+  ShowVeins: true,             // toggled with 'v'
+  ShowVeinTips: false,         // toggled with 't'
+  ShowAttractionZones: false,  // toggled with 'a'
+  ShowKillZones: false,        // toggled with 'k'
+  ShowInfluenceLines: false,   // toggled with 'i'
+  ShowBounds: true,            // toggled with 'b'
+  ShowObstacles: true,         // toggled with 'o'
 
   // Modes
   VeinRenderMode: 'Lines',  // draw vein segments as "Lines" or "Dots"
