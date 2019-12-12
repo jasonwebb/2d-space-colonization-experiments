690b01888fe30438e6775f11ddb0bc2c427aa879
Jason Webb <zen.webb@gmail.com>
2019-12-11 19:44:45 -0600

    Adding initial implementation of source distribution along path

--- sources-on-paths/js/entry.js
+++ sources-on-paths/js/entry.js
@@ -1,15 +1,16 @@
 import * as Vec2 from 'vec2';
 import Network from '../../core/Network';
-import { getRandomSources, getGridOfSources } from '../../core/SourcePatterns';
 import VeinNode from '../../core/VeinNode';
 import Path from '../../core/Path';
-import SVGLoader from '../../core/SVGLoader';
 import { setupKeyListeners } from '../../core/KeyboardInteractions';
+import AuxinSource from '../../core/AuxinSource';
 
 let canvas, ctx;
 let network;
 
 let currentPath;
+
+let yPosition = 0;
 
 // Create initial conditions for simulation
 let setup = () => {
@@ -42,11 +43,12 @@ let resetNetwork = () => {
   let setupPath = () => {
     const cx = window.innerWidth/2;
     const cy = window.innerHeight/2;
+    yPosition = cy;
 
     currentPath = new Path(
       [
-        [cx - 400, cy + 200],
-        [cx + 400, cy + 200]
+        [cx - 400, cy + 400],
+        [cx + 400, cy + 400]
       ],
       'Bounds',
       ctx
@@ -57,26 +59,79 @@ let resetNetwork = () => {
   let addRootVeins = () => {
     const cx = window.innerWidth/2;
     const cy = window.innerHeight/2;
+
+    network.addVeinNode(
+      new VeinNode(
+        null,
+        new Vec2(
+          cx,
+          cy + 400
+        ),
+        true,
+        ctx
+      )
+    )
   }
 
   let movePath = () => {
-    if(!network.settings.IsPaused) {
-      currentPath.moveBy(0,-.5);
+    if(!network.settings.IsPaused && yPosition > -800) {
+      currentPath.moveBy(0,-2);
+      yPosition = currentPath.origin.y;
     }
   }
 
   let generateSourcesOnPath = () => {
     let sources = [];
+    const sourceSpacing = 10;
+    let totalPathLength = currentPath.getTotalLength();
+    const numberOfSources = totalPathLength / sourceSpacing;
+    let previousSegmentRemainder = 0;
+
+    // For each path segment ...
+    for(let i=1; i<currentPath.polygon.length; i++) {
+      const point0 = Vec2(currentPath.polygon[i-1][0] + currentPath.origin.x, currentPath.polygon[i-1][1] + currentPath.origin.y);
+      const point1 = Vec2(currentPath.polygon[i][0] + currentPath.origin.x, currentPath.polygon[i][1] + currentPath.origin.y);
+      const currentSegmentLength = point1.distance(point0);
+      const availableLength = currentSegmentLength - previousSegmentRemainder;
+
+      // We can fit at least one source onto this segment
+      if(availableLength >= sourceSpacing) {
+        let segmentDirection = point1.subtract(point0, true).normalize();
+
+        // How many sources can we fit onto this segment?
+        const numSources = Math.floor(availableLength / sourceSpacing);
+
+        // Create as many auxin sources as we can
+        for(let sourceIndex=0; sourceIndex<=numSources; sourceIndex++) {
+          sources.push(
+            new AuxinSource(
+              point0.add(segmentDirection.multiply(sourceSpacing * sourceIndex, true), true),
+              ctx
+            )
+          );
+        }
+
+        // Store remainder of segment length to offset next segment's source placement
+        previousSegmentRemainder = numberOfSources * sourceSpacing;
+
+      // Can't fit any sources onto this segment, so accumulate the length (previous segments might've also been too short)
+      } else {
+        previousSegmentRemainder += currentSegmentLength;
+      }
+    }
 
     network.sources = sources;
   }
 
 // Main program loop
 let update = (timestamp) => {
-  movePath();
-  generateSourcesOnPath();
+  if(!network.settings.IsPaused) {
+    movePath();
+    generateSourcesOnPath();
 
-  network.update();
+    network.update();
+  }
+
   network.draw();
   currentPath.draw();
 
